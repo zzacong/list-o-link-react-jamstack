132d8e1f39ef43f37023cf27fbcca5141fb5579c
zzacong <61817066+zzacong@users.noreply.github.com>
2022-11-25 17:18:21 +1100

    with edge function

--- api/links/[linkId].mjs
+++ api/links/[linkId].mjs
@@ -1,31 +1,41 @@
 import sendQuery from '../../lib/utils/sendQuery.mjs'
 import { DELETE_LINK, UPDATE_LINK } from '../../lib/utils/linkQuery.mjs'
+import { json } from '../../lib/utils/edge-response.mjs'
 
-export default async function handler(req, res) {
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function handler(req) {
   try {
     // PUT /api/links/[linkId]
     if (req.method === 'PUT') {
-      const { linkId } = req.query
-      const { name, url, description, archived } = req.body
+      const linkId = getLinkId(req)
+      const { name, url, description, archived } = await req.json()
       const variables = { name, url, description, archived, id: linkId }
       const { updateLink: updatedLinked } = await sendQuery(
         UPDATE_LINK,
         variables
       )
-      return res.status(200).json(updatedLinked)
+      return json(updatedLinked)
     }
 
     // DELETE /api/links/[linkId]
     if (req.method === 'DELETE') {
-      const { linkId } = req.query
+      const linkId = getLinkId(req)
       const { deleteLink: deletedLink } = await sendQuery(DELETE_LINK, {
         id: linkId,
       })
-      return res.status(200).json(deletedLink)
+      return json(deletedLink)
     }
   } catch (error) {
     console.error(error)
-    return res.status.status(500).json({ error: 'Something went wrong' })
+    return json({ error: 'Something went wrong' }, 500)
   }
-  return res.status(405).json({ error: 'Method not supported' })
+  return json({ error: 'Method not supported' }, 405)
 }
+
+// const getLinkId = req =>
+//   new URL(req.url).pathname.split('/').slice(-1)[0]
+
+const getLinkId = req => new URL(req.url).searchParams.get('linkId')
--- api/links/index.mjs
+++ api/links/index.mjs
@@ -1,24 +1,29 @@
 import sendQuery from '../../lib/utils/sendQuery.mjs'
 import { GET_LINKS, CREATE_LINK } from '../../lib/utils/linkQuery.mjs'
+import { json } from '../../lib/utils/edge-response.mjs'
 
-export default async function (req, res) {
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function (req) {
   try {
     if (req.method === 'GET') {
       const data = await sendQuery(GET_LINKS)
-      return res.status(200).json(data.allLinks.data)
+      return json(data.allLinks.data)
     }
     if (req.method === 'POST') {
-      const { name, url, description } = req.body
+      const { name, url, description } = await req.json()
       const variables = { name, url, description, archived: false }
       const { createLink: createdLink } = await sendQuery(
         CREATE_LINK,
         variables
       )
-      return res.status(201).json(createdLink)
+      return json(createdLink, 201)
     }
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ error: 'Something went wrong' })
+    return json({ error: 'Something went wrong' }, 500)
   }
-  return res.status(405).json({ error: 'Method not supported' })
+  return json({ error: 'Method not supported' }, 405)
 }
